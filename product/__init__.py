28167487d83e09c22cf254f629e35bfaf112f543
DmitryPimenov <dmitrynetmail@gmail.com>
2020-12-03 04:28:06 +0300

    init commit

--- product/__init__.py
+++ product/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/product'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:D?f4nX#3@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
